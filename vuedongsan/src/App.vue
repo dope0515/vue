484099e12fb29147d115bab206129c1821a8c150
dope0515 <61546301+dope0515@users.noreply.github.com>
2023-03-07 16:52:12 +0900

    .

---
+++ vuedongsan/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <!--메뉴-->
+  <div class="menu">
+    <a href="javascript:void(0)" v-for="(menuList,i) in menus" :key="i">{{ menuList }}</a>
+  </div>
+  <!--//메뉴-->
+  <!--원룸 가격 리스트-->
+  <div v-for="(a,i) in products" :key="a">
+    <h4>{{ products[i] }} 원룸</h4>
+    <p>{{ price[i] }} 만원</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      price : [60,100, 130],
+      products: ['역삼동원룸','천호동원룸','마포구원룸'],
+      menus : ['Home','Procuts','About'],
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background: darkslateblue;
+  padding:15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: #FFFFFF;
+  padding: 10px;
+}
+</style>
